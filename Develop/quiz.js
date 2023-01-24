035023f5e45d326e739b236d9ef274ff382e5b51
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-24 12:36:39 -0500

    Framework for JS
    The html is where I want it but I still need to add a few things. The framework for the javascript is done with exception for maybe a couple of things.

---
+++ Develop/quiz.js
@@ -0,0 +1,42 @@
+var questions = [
+  {
+    title:
+    choices: []
+    answer:
+  },
+
+var quiz = document.querySelector("#quiz");
+var countdown = document.querySelector("#time");
+var choices = document.querySelector("#choices");
+var start = document.querySelector("#start");
+
+// quiz state variables
+var questionIndex = 0;
+var time = questions.length * 15;
+var timer;
+
+function startQuiz() {
+  
+}
+
+function nextQuestion() {
+
+}
+
+function questionChoice() {
+
+}
+
+function endQuiz() {
+
+}
+
+function clock() {
+
+}
+
+function highscore() {
+
+}
+
+
