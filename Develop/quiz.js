7d26325ffdbc7d6c685b4bdd17eb871929e44617
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-24 13:11:22 -0500

    Second Iteration
    I've began to create the functions for the quiz. I'm not entirely sure if this will function but I'm still slowly adding functions and yet to add the questions. However I believe this is a solid line of logic.

--- Develop/quiz.js
+++ Develop/quiz.js
@@ -16,14 +16,58 @@ var time = questions.length * 15;
 var timer;
 
 function startQuiz() {
-  
+  var homeStart = document.getElementById("home");
+  homeStart.setAttribute("class", "next");
+
+  quiz.removeAttribute("class");
+
+  timer = setInterval(clock, 1000);
+
+  countdown.textContent = time;
+
+  nextQuestion();
 }
 
 function nextQuestion() {
+  var currentQuestion = questions[questionIndex];
 
+  var titleQuestion = document.getElementById("questions");
+  titleQuestion.textContent = currentQuestion.title;
+
+  choices.innerHTML = "";
+
+  currentQuestion.choices.forEach(function(choice, i)) {
+    var choiceEl = document.createElement("button");
+    choiceEl.setAttribute("class", "choice");
+    choiceEl.setAtrribute("value", choice);
+
+    choiceEl.textContent = i + 1 + ". " + choice;
+
+    choiceEl.onclick = questionChoice;
+
+    choices.appendChild(choiceEl);
+  }
 }
 
+
 function questionChoice() {
+  if (this.value !== questions[questionsIndex].answer) {
+
+    time -= 15;
+
+    if (time < 0) {
+      time = 0;
+    }
+  }
+
+  questionIndex++;
+
+  if (questionIndex === questions.length) {
+    quizEnd();
+  } else {
+    nextQuestion
+  }
+
 
 }
 
