bc28ba0f9e8480c998865be078a03a180352a5f0
Aaron Dreyer <aaronottaway@gmail.com>
2023-01-25 13:10:33 -0500

    score.js fix
    I fixed the javascript for the highscores by changing the names of some functions and changing the forEach function to an unordered list. I also missed linked the back button sonow the back button works as well.

--- Develop/score.js
+++ Develop/score.js
@@ -1,28 +1,29 @@
-function printhighscores() {
-    var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
+function printScores() {
+  var highscores = JSON.parse(window.localStorage.getItem("highscores")) || [];
 
-    highscores.sort(function(a, b) {
-      return score;
+  highscores.sort(function(a, b) {
+    return b.score - a.score;
+  });
+
+  highscores.forEach(function(score) {
+      var liTag = document.createElement("li");
+      liTag.textContent = score.name + " - " + score.score;
+  
+      var ulEl = document.getElementById("highscores");
+      ulEl.appendChild(liTag);
     });
+  }
   
-    highscores.forEach(function(score) {
-        var liTag = document.createElement("li");
-        liTag.textContent = score.name + " - " + score;
-    
-        var olEl = document.getElementById("highscores");
-        olEl.appendChild(liTag);
-      });
-    }
-    
-    function clearhighscores() {
-      window.localStorage.removeItem("highscores");
-      window.location.reload();
-    }
-    
-    document.getElementById("clear").onclick = clearhighscores;
-    
-    printhighscores();
+  function clearScores() {
+    window.localStorage.removeItem("highscores");
+    window.location.reload();
+  }
   
-    back.addEventListener("click", function () {
-        window.location.replace(".index.html");
-    })
+  document.getElementById("clear").onclick = clearScores;
+  
+  printScores();
+
+  back.addEventListener("click", function () {
+      window.location.replace("./index.html");
+  })
+
